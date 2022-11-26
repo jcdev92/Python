95eb7f5b8dcee7e8a202499db9137f0e29a8714a
jcdev92 <jcdeveloper92@gmail.com>
2022-11-26 00:07:21 -0400

    new changes added

---
+++ Basico/def.py
@@ -0,0 +1,12 @@
+# def dummy_fucnt(name, lname = ''):
+#     return 'Hi ' + name + ' ' + lname + '! ' + str(4)
+#
+# def args(*args):
+#     print(args)
+#     return args[0]
+#
+# def main():
+#     print(args('John', 'Doe'))
+#
+# if __name__ == "__main__":
+#     main()
---
+++ Basico/vars.py
@@ -0,0 +1,44 @@
+# country = "US"
+# state = "CA"
+
+# VARIABLES ARE A OBJECTS REFERENCES IN MEMORY (POINTERS)
+# THESE OBJECTS HAVE A TYPE, A VALUE AND A ID
+# THE ID IS THE ADDRESS IN MEMORY WHERE THE OBJECT IS STORED
+# THE VALUE IS THE VALUE THAT WAS ASSIGNED TO THE OBJECT
+# THE TYPE IS THE DATA TYPE OF THE OBJECT
+
+# print(country) # US  // value
+# print(id(country)) # 140735000000000 // memory address
+# print(type(country)) # <class 'str'> // type
+
+# print(country) # US
+# print(id(state)) # 140735000000000
+# print(type(state)) # <class 'str'>
+
+# country_code = int('52') # 52
+# print(country_code, type(country_code)) # 52 <class 'int'>
+
+# country_code = float('52.5') # 52.5
+
+# print(country_code, type(country_code)) # 52.5 <class 'float'>
+
+# country_code = str(52) # '52'
+
+# print(country_code, type(country_code)) # 52 <class 'str'>
+
+# country_code = bool(0) # False
+
+# print(country_code, type(country_code)) # False <class 'bool'>
+
+# var_1, var_2, var_3 = 1, 2, 3 # 1 2 3
+
+# print(var_1, var_2, var_3) # 1 2 3
+
+# var_1, var_2, var_3 = var_2, var_3, var_1 # 2 3 1
+
+# print(var_1, var_2, var_3) # 2 3 1
+
+# var_1 = var_2 = var_3 = 1 # 1 1 1
+
+# print(var_1, var_2, var_3) # 1 1 1
+
