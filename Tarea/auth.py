2998dbd3afc7553a860df80ced9569b8666feba8
jcdev92 <jcdeveloper92@gmail.com>
2022-12-11 15:16:43 -0400

    new virtual env

--- Tarea/auth.py
+++ Tarea/auth.py
@@ -2,3 +2,11 @@ from Flask import Flask, request
 
 app = Flask(__name__)
 
+
+@app.route('/auth')
+def auth():
+    return {'message': 'Hello World'}, 200
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=9001, debug=True)
