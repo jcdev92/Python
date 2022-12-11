0439d33021ae76a20e867339b8fe7bef41240558
jcdev92 <jcdeveloper92@gmail.com>
2022-12-11 13:48:41 -0400

    poke APIRestfull tarea

--- Tarea/app.py
+++ Tarea/app.py
@@ -1,17 +1,33 @@
 from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
 
-def controller_poke():
-    return {
-        'controller_poke': True
-    }
+def controller_poke(headers):
+    try:
+        endpoint_poke_api = headers['endpoint_poke_api']
+        param_ability_name = headers['param_ability_name']
+        ability_range = headers['ability_range']
+        ability_range = int(ability_range)
+        response = requests.get(endpoint_poke_api)
+        response = response.json()
+        abilities = response['abilities'][ability_range]
+        ability_name = abilities['ability']['name']
+        print(ability_name)
+    except Exception as e:
+        return {'error': e.args[0]}, 400
+    else:
+        if param_ability_name == ability_name:
+            return {'exists_ability': True}, 200
+        else:
+            return {'exists_ability': False}, 200
+
 
 
 @app.route('/poke')
 def poke():
-    response = controller_poke()
+    response = controller_poke(request.headers)
     return response
 
 
