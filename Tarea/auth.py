97c738aa679b736d368eeaac52422e65bd8ca024
jcdev92 <jcdeveloper92@gmail.com>
2022-12-11 22:38:55 -0400

    auth anadido

--- Tarea/auth.py
+++ Tarea/auth.py
@@ -1,14 +1,44 @@
+import datetime
+from flask import Flask, request, after_this_request
 from flask_api import status
-from flask import Flask, request
+import jwt
 
 app = Flask(__name__)
 
 status_code = status.HTTP_200_OK
 
 
+@app.route('/login')
+def login():
+    payload = {
+        'id': request.headers['uuid'],
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60),
+        'iat': datetime.datetime.utcnow()
+    }
+
+    token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+    @after_this_request
+    def add_cookie(response):
+        response.set_cookie('jwt', token)
+        return response
+
+    resp = {'jwt': token}
+    return resp
+
+
 @app.route('/auth')
 def auth():
-    return {'status': 'run'}, 200
+    try:
+        token = request.cookies.get('jwt')
+        payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        if payload['id'] != request.headers['uuid']:
+            return {'Authenticate': False}, status.HTTP_401_UNAUTHORIZED
+
+    except jwt.ExpiredSignatureError:
+        return {'Authenticate': False}, status.HTTP_401_UNAUTHORIZED
+
+    return {'Authenticate': True}, status.HTTP_200_OK
 
 
 if __name__ == '__main__':
