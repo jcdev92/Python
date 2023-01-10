3f16978343e0fbe26bc2e9be68e0c223e8c9f5f6
jcdev92 <jcdeveloper92@gmail.com>
2023-01-09 22:04:07 -0400

    hangman game completed

--- games/hangman/hangman.py
+++ games/hangman/hangman.py
@@ -1,6 +1,7 @@
 import random
 from hangman_arts import stages, logo
 from hangman_words import word_list
+import os
 
 # words = ["autana", "roraima", "corolla", "arepa", "veneco", "vinotinto"]
 
@@ -8,31 +9,41 @@ selected_word = random.choice(word_list)
 
 blanks = []
 matched = ""
+formated = ""
 lives = 6
+clear = lambda: os.system("clear")
+hangman = ""
+
 
 for blank in selected_word:
-    blanks.append(" - ")
+    blanks.append(" _ ")
 
 print(logo)
 print("please welcome to the hangman game, enter a letter until you match the secret word or lose all your lives")
+
 while matched != selected_word and lives > 0:
-    print(f"\n   {matched}  \n")
+    print(f"\n   {formated}  \n")
     letter = input("please enter a letter:\n").lower()
+    clear()
     if selected_word.find(letter) >= 0:
         for i in range(0, len(selected_word)):
             if selected_word[i] == letter:
                 blanks[i] = letter
+        print(f"HIT! the letter: '{letter}' is in the secret word, keep going until you win or you die")
     else:
         lives -= 1
         hangman = stages[lives]
-        print(f"\nwrong! you gussed the letter: {letter}, and that's not in the secret word, you loose a life")
-        print(hangman)
+        print(f"\nwrong! you gussed the letter: '{letter}', and that's not in the secret word, you loose a life")
 
+    if hangman:    
+        print(hangman)
+    
+    formated = ' '.join(blanks)
     matched = "".join(blanks)
 
 if lives == 0:
     print("game over, you are dead")
 elif matched == selected_word:
-    print(f"\n{matched}")
+    print(f"\n The WORD is \n{matched}")
     print("\nCongrats!! You Win!")
     
